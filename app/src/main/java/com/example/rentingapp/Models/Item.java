0a9b9e35c8fb8c9a6a5cf1e8aa09c232de6fdaa3
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-12 20:33:45 -0500

    Created setters for Item Model (Parse)

--- app/src/main/java/com/example/rentingapp/Models/Item.java
+++ app/src/main/java/com/example/rentingapp/Models/Item.java
@@ -1,9 +1,43 @@
 package com.example.rentingapp.Models;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 @ParseClassName("Item")
 public class Item extends ParseObject {
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_OWNER = "owner";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_IMAGES = "images";
+    public static final String KEY_IS_RENTED = "isRented";
+    public static final String KEY_CREATED_KEY = "createdAt";
+    public static final String KEY_PRICE = "price";
 
+    public void setOwner(ParseUser owner) {
+        put(KEY_OWNER, owner);
+    }
+
+    public void setTitle(String title) {
+        put(KEY_TITLE, title);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public void setImages(List<ParseFile> images) {
+        put(KEY_IMAGES, images);
+    }
+
+    public void setIsRented(Boolean value) {
+        put(KEY_IS_RENTED, value);
+    }
+
+    public void setPrice(float price){
+        put(KEY_PRICE, price);
+    }
 }
