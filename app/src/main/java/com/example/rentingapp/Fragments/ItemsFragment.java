5ee632b5f774fc4cc8986d91d0a9bb363d58d056
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-16 11:52:56 -0500

    using pageviews and sectionspager

--- app/src/main/java/com/example/rentingapp/Fragments/ItemsFragment.java
+++ app/src/main/java/com/example/rentingapp/Fragments/ItemsFragment.java
@@ -5,20 +5,24 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.rentingapp.Adapters.SectionsPagerAdapter;
 import com.example.rentingapp.CreateItemActivity;
 import com.example.rentingapp.MultipleImagesActivity;
 import com.example.rentingapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +31,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
  */
 public class ItemsFragment extends Fragment {
     FloatingActionButton fabComposeItem;
+    ViewPager viewPager;
+    TabLayout tabs;
 
 
     public ItemsFragment() {
@@ -47,9 +53,17 @@ public class ItemsFragment extends Fragment {
 
     }
 
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getContext(), getChildFragmentManager());
+        viewPager = view.findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabs = view.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
         fabComposeItem = view.findViewById(R.id.fabComposeItem);
         fabComposeItem.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,15 +72,6 @@ public class ItemsFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
-
-        /*NavController navController = Navigation.findNavController(view);
-        AppBarConfiguration appBarConfiguration =
-                new AppBarConfiguration.Builder(navController.getGraph()).build();
-        Toolbar toolbar = view.findViewById(R.id.toolbar);
-
-        NavigationUI.setupWithNavController(
-                toolbar, navController, appBarConfiguration);*/
 
     }
 }
