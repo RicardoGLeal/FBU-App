34334ff6f3e35b454d5b5ad16afb0689941317ca
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-07-19 11:46:19 -0500

    Profile Fragment Created
    Now the user can see in their profile their general information, as well as a RecyclerView with the items they have published

---
+++ app/src/main/java/com/example/rentingapp/Adapters/ItemsProfileAdapter.java
@@ -0,0 +1,97 @@
+package com.example.rentingapp.Adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.rentingapp.Fragments.ItemDetailsFragment;
+import com.example.rentingapp.Models.Item;
+import com.example.rentingapp.R;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class ItemsProfileAdapter extends RecyclerView.Adapter<ItemsProfileAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Item> items;
+
+    public ItemsProfileAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_item_image, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsProfileAdapter.ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    /**
+     * Clean all elements of the recycler
+     */
+    public void clear() {
+        items.clear();
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ImageView ivImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Item item) {
+            //Bind the post data to the view elements
+            ParseFile image = item.getImages().get(0);
+            //condition to check if there is an image attached
+            if (image != null) {
+                Glide.with(context).load(image.getUrl()).into(ivImage);
+            }
+        }
+
+        /**
+         * OnClickListener of each item. It opens the PostDetailsFragment with the details of the post clicked.
+         * @param v
+         */
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                AppCompatActivity activity = (AppCompatActivity) context;
+                Fragment fragment;
+                fragment = new ItemDetailsFragment(items.get(position));
+                ((AppCompatActivity) context).getSupportFragmentManager();
+                activity.getSupportFragmentManager().beginTransaction().replace(R.id.flContainer, fragment).commit();
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/rentingapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/rentingapp/Fragments/ProfileFragment.java
@@ -2,14 +2,33 @@ package com.example.rentingapp.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.rentingapp.Adapters.ItemsProfileAdapter;
+import com.example.rentingapp.Models.Item;
+import com.example.rentingapp.Models.User;
 import com.example.rentingapp.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.rentingapp.Controllers.ImagesController.loadCircleImage;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,45 +36,26 @@ import com.parse.ParseUser;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
+    public static final String TAG = "ProfileFragment";
+    private ParseUser user;
+    private ImageView ivProfileImage;
+    private TextView tvPersonName, tvPersonDescription;;
+    private RecyclerView rvItems;
+    protected List<Item> allItems;
+    private ItemsProfileAdapter adapter;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment(ParseUser currentUser) {
-        // Required empty public constructor
+    public ProfileFragment(ParseUser user) {
+        this.user = user;
     }
-
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
+     * Create a new instance of this fragment using the provided parameters.
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static ProfileFragment newInstance(String param1, String param2) {
         ProfileFragment fragment = new ProfileFragment(ParseUser.getCurrentUser());
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,5 +63,52 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Get references
+        ivProfileImage = view.findViewById(R.id.ivProfileImage);
+        tvPersonName = view.findViewById(R.id.tvPersonName);
+        tvPersonDescription = view.findViewById(R.id.tvPersonDescription);
+        rvItems = view.findViewById(R.id.rvItems);
+        //Asign values
+        loadCircleImage(getContext(), user.getParseFile(User.KEY_PROFILE_PICTURE), ivProfileImage);
+        tvPersonName.setText(user.getString(User.KEY_NAME));
+        tvPersonDescription.setText(user.getString(User.KEY_DESCRIPTION));
+        allItems = new ArrayList<>();
+        adapter = new ItemsProfileAdapter(getContext(), allItems);
+        rvItems.setAdapter(adapter);
+        rvItems.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        queryItems();
+    }
+
+    private void queryItems() {
+        // Specify which class to query
+        ParseQuery<Item> query = ParseQuery.getQuery(Item.class);
+        //include the user of the post
+        query.include(Item.KEY_OWNER);
+        //Limiting the number of posts getting back.
+        query.setLimit(20);
+        //the items created most recently will come first and the oldest ones will come last.
+        query.addDescendingOrder(Item.KEY_CREATED_AT);
+        query.whereEqualTo(Item.KEY_OWNER, user);
+
+        // Retrieve all the posts
+        query.findInBackground(new FindCallback<Item>() {
+            @Override
+            public void done(List<Item> items, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Item item: items) {
+                    Log.i(TAG, "Items: " + item.getDescription());
+                }
+                allItems.addAll(items);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
